f0da102e9464d2b015e9b539184d3132204010cb
nmocherniuk <nazar.mocherniuk@gmail.com>
2024-12-09 13:45:17 +0200

    chore: update next.config.js to handle SVGs, configure image domains and intall svgr/webpack as dependency

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,40 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgo: true, // Оптимізація SVG
+            svgoConfig: {
+              plugins: [
+                {
+                  name: "removeViewBox",
+                  active: false, // Не видаляти атрибут `viewBox`
+                },
+              ],
+            },
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "orisdi.com",
+      "www.everich.com",
+      "content.rozetka.com.ua",
+      "m.media-amazon.com",
+      "30316cca.rocketcdn.me",
+      "vest.in.ua",
+    ],
+  },
+};
 
 export default nextConfig;
